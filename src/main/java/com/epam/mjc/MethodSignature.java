7883e4fec3ef57bdad8566f5a9be5513a9856e99
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:05:15 +0300

    Update MethodSignature.java

--- src/main/java/com/epam/mjc/MethodSignature.java
+++ src/main/java/com/epam/mjc/MethodSignature.java
@@ -1,51 +1,22 @@
 package com.epam.mjc;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MethodSignature {
+
+    private String methodName;
+    private List<Argument> arguments;
     private String accessModifier;
     private String returnType;
-    private String methodName;
-    private final List<Argument> arguments;
 
-    public MethodSignature(String methodName, List<Argument> arguments) {
+    public MethodSignature(String methodName, List<Argument> arguments, String accessModifier, String returnType) {
         this.methodName = methodName;
         this.arguments = arguments;
-    }
-
-    public MethodSignature(String methodName) {
-        this(methodName, new ArrayList<>());
-    }
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
-
-    public String getAccessModifier() {
-        return accessModifier;
-    }
-
-    public void setAccessModifier(String accessModifier) {
         this.accessModifier = accessModifier;
-    }
-
-    public String getReturnType() {
-        return returnType;
-    }
-
-    public void setReturnType(String returnType) {
         this.returnType = returnType;
     }
 
-    public List<Argument> getArguments() {
-        return arguments;
-    }
-
+    // Аргумент метода
     public static class Argument {
         private String type;
         private String name;
@@ -59,16 +30,25 @@ public class MethodSignature {
             return type;
         }
 
-        public void setType(String type) {
-            this.type = type;
-        }
-
         public String getName() {
             return name;
         }
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    // Геттеры
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public List<Argument> getArguments() {
+        return arguments;
+    }
+
+    public String getAccessModifier() {
+        return accessModifier;
+    }
+
+    public String getReturnType() {
+        return returnType;
     }
 }
